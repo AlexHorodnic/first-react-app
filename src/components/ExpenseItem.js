db547e36628e529929143dd7a24c9379b9bd49ae
AlexHorodnic <alexhorodnic@protonmail.com>
2022-06-21 12:21:49 +0300

    used props to fill component with data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const expenseDate = new Date(2022, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 120.20;
+
   return (
     <div className='expense-item'>
-      <div>June 21st 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Food</h2>
-        <div className='expense-item__price'>$120.20</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
